4c16ddaa57b64acb7406203589eebd9bf0cd3fb4
kirjavascript <snkenjoi@gmail.com>
2018-12-11 22:21:07 +0000

    poop

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
 wasm-string
 ======
 
-Minimal example to export a string from Rust to JS
+export a string from Rust to JS
 
 setup: https://www.hellorust.com/setup/wasm-target/
-
-todo
-====
-
-https://stackoverflow.com/questions/49014610/passing-a-javascript-string-to-a-rust-function-compiled-to-webassembly/49020435  
-pass pointer and length in a single u64  
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use std::ffi::CString;
 use std::os::raw::c_char;
 
+// imports from js
+
 #[no_mangle]
 extern "C" {
     fn stack_push(num: usize);
     fn console_log_stack();
 }
+
+// https://stackoverflow.com/questions/49014610/passing-a-javascript-string-to-a-rust-function-compiled-to-webassembly/49020435
+
+// exports to js
 
 #[no_mangle]
 pub extern "C" fn deallocate_string(ptr: *mut c_char) {
@@ -18,27 +24,31 @@ macro_rules! export_string {
         pub extern "C" fn $name() {
             let rust_string = $exec;
             let rust_string_length = rust_string.len();
-            let c_string = CString::new(rust_string)
+            let c_string = std::ffi::CString::new(rust_string)
                 .expect("must be a valid C string");
             unsafe { stack_push(rust_string_length); }
-            unsafe { stack_push(c_string.into_raw() as *mut c_char as usize); }
+            unsafe { stack_push(c_string.into_raw() as usize); }
         }
     }
 }
 
+// helpers
+
 pub fn console_log(string: &str) {
-    for c in string.chars() {
-        unsafe { stack_push(c as usize); }
+    #[cfg(target_arch = "wasm32")]
+    {
+        for c in string.chars() {
+            unsafe { stack_push(c as usize); }
+        }
+        unsafe { console_log_stack(); }
     }
-    unsafe { console_log_stack(); }
 }
 
+// examples
 
+export_string!(TEST_STRING, get_string());
 fn get_string() -> &'static str {
     "test string"
 }
 
-export_string!(string_export, get_string());
-
-fn main() {
-}
+fn main() { }
